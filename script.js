38081657a6ae4ebc5818c0a571148aac6460912b
abhishek kumar hansda <118197040+abhishekkrhansda@users.noreply.github.com>
2026-09-13 16:46:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 function volume_sphere() {
-    //Write your code here
-  
-} 
+    let r = document.querySelector("#radius");
+    let v = document.querySelector("#volume");
+
+    let rVal = Number(r.value);
+
+    if (rVal >= 0) {
+        let vol = (4 / 3) * Math.PI * rVal * rVal * rVal;
+        v.value = vol;
+    }
+
+    return false;
+}
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
